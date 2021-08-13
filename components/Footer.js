0b8c7c882891d511c199765f105bfeeca41caead
rupal <rupalmalik3@gmail.com>
2021-08-13 12:50:49 +0530

    chore: updated footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,64 +1,51 @@
 import React from "react";
 import Image from "next/image";
+import { FacebookIcon, SearchIcon, LinkedIn } from "/assets/icons";
 const Footer = () => {
   return (
-    <>
-      <div className="flex-container">
-        <div className="footer_section">
-          <Image
-            src="/assets/images/dailykit_logo.svg"
-            width="50"
-            height="60"
-            style={{ paddingBottom: "1rem" }}
-            alt="footer_logo"
-          />
-          <p>
-            DailyKit is a Meal Kit Technology Platform
-            <br />
-            Our software suite - recipe management, manufacturing solution,
-            sales and marketing platforms and delivery integrations are
-            everything that your company needs to grow.
-          </p>
-        </div>
-        <div className="footer_section">
-          <h3>ABOUT US</h3>
-          <p>
-            <a href="/Aboutus/Ourphilosophy"> DailyKit Philosophy</a>
-          </p>
-        </div>
-
-        <div className="footer_section">
-          <h3>START A MEAL KIT BRAND</h3>
-
-          <li>
-            <a href="/restaurants/cafes"> QSR & Cafes</a>
-          </li>
-          <li>
-            <a href="/restaurants/fine-dine-restaurants">
-              Casual and Fine Dining
-            </a>
-          </li>
-          <li>
-            <a href="/cloud_kitchen">Cloud Kitchen</a>
-          </li>
-          <li>
-            <a href="/bakerypage">Bakery</a>
-          </li>
-        </div>
-
-        <div className="footer_section">
-          <h3>Contact us</h3>
-
-          <li>Rishi@dailykit.org</li>
-
-          <li>+13125210704</li>
+    <div style={{ background: "#111B2B", color: "#fff", height: "400px" }}>
+      <div className="container flex-container" style={{ flexDirection: "row" }}>
+        <div className="row">
+          <div className="col-md-12 col-sm-12" style={{ margin: "60px 0 -40px 0" }}>
+            <Image
+              src="/assets/images/Logo.png"
+              width="170"
+              height="45"
+              style={{ paddingBottom: "1rem" }}
+              alt="footer_logo"
+            /></div>
+        </div></div>
+      <div className="container" style={{ flexDirection: "row", marginBottom: "40px" }} >
+        <div className="row">
+          <h5 className="col-md-2 col-sm-8 footer-links">
+            About Us
+          </h5>
+          <h5 className="col-md-3 col-sm-8 footer-links">
+            Terms & Conditions
+          </h5>
+          <h5 className="col-md-2 col-sm-8 footer-links">
+            Privacy Policy
+          </h5>
+          <h5 className="col-md-3 col-sm-8 footer-links">
+            Refund & Cancellation
+          </h5>
+          <h5 className="col-md-2 col-sm-8 footer-links">
+            Contact us
+          </h5>
         </div>
       </div>
+      <div className="container flex-container" style={{ flexDirection: "row", marginBottom: "80px" }}>
+        <div className="row">
+          <div className="col-md-12 col-sm-12">
+            <FacebookIcon /> &nbsp; &nbsp;<SearchIcon /> &nbsp; &nbsp;<LinkedIn />
+          </div>
+        </div>
 
+      </div>
       <div className="copyright">
         Copyright © 2021 DailyKit Inc. All Rights Reserved.
       </div>
-    </>
+    </div>
   );
 };
 export default Footer;
