10d6373538c7f00c322d40cc66a191478f773610
rupal <rupalmalik3@gmail.com>
2021-08-19 16:29:39 +0530

    fix model issue

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,8 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { useAuth } from "../store/auth";
 import { Styles} from "../components/sections/Header/styled";
-import CompleteSignup from "./subcomponents/shared/CompleteSignup";
 export const Navbar = () => {
   const location = useRouter();
   const { user, authenticated, logout } = useAuth();
@@ -35,7 +34,7 @@ export const Navbar = () => {
                   {user?.organization?.onboardStatus != "FINISH_SETUP" ? (
                   <>
                   {/* for mobile */}
-                  <a type="button" id="exampleModal" className="btn-style-thirteen" data-bs-toggle="modal" data-bs-target="#exampleModal" href="/">
+                  <a type="button" id="exampleModal-1" className="btn-style-thirteen" data-bs-toggle="modal" data-bs-target="#exampleModal" href="/">
                   <span className="navbar-toggler-icon light"></span> 
                   </a>
                 <CompleteSignup/>
@@ -128,6 +127,57 @@ export const Navbar = () => {
     
       </div>
     </nav>
+  );
+}
+ const CompleteSignup = () => {
+  const location = useRouter();
+  return (
+    <>
+      <div
+        className="modal"
+        id="exampleModal"
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-dialog-centered mt-4" style={{ background: "rgba(0, 0, 0, 0.7)" }}>
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                Complete your Signup
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <div class="container-fluid">
+                <div class="row justify-content-center">
+                  <div class="col-md-6 ml-auto">
+                    <Image width="300px"
+                    height="300px"
+                    src='/assets/images/LoginPage.png'
+                    alt="login-page" /></div>
+                </div>
+              </div>
+            </div>
+
+            <div className="modal-footer">
+              <Styles.ghostButton style={{
+                background: "#111b2b",
+                color: "white", display: "inline", paddingRight: "1.3rem", paddingLeft: "1.3rem", paddingTop: "10px",
+                paddingBottom: "10px", fontSize: "17px", marginRight: "40px"
+              }}
+                className="nav-link"
+                onClick={() => location.push('/onboard/signup/company')}>Complete your Signup</Styles.ghostButton >
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
--- components/subcomponents/shared/CompleteSignup.jsx
+++ components/subcomponents/shared/CompleteSignup.jsx
@@ -1,54 +1,58 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import Image from "next/image";
-import { Styles } from "../../sections/Header/styled"
-export default class CompleteSignup extends PureComponent {
-  render() {
-    return (
-      <>
-        <div
-          className="modal fade"
-          id="exampleModal"
-          tabIndex="-1"
-          aria-labelledby="exampleModalLabel"
-          aria-hidden="true"
-        >
-          <div className="modal-dialog modal-dialog-centered mt-4" style={{background:"rgba(0, 0, 0, 0.7)"}}>
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title" id="exampleModalLabel">
-                  Complete your Signup
-                </h5>
-                <button
-                  type="button"
-                  className="btn-close"
-                  data-bs-dismiss="modal"
-                  aria-label="Close"
-                ></button>
-              </div>
-              <div className="modal-body">
-                <div class="container-fluid">
-                  <div class="row justify-content-center">
-                    <div class="col-md-6 ml-auto"><Image width="300px"
-                      height="300px"
-                      src='/assets/images/LoginPage.png'
-                      alt="login-page" /></div>
-                  </div>
+import { useRouter } from "next/router";
+
+import { Styles } from "../../sections/Header/styled";
+
+export const CompleteSignup = () => {
+  const location = useRouter();
+  return (
+    <>
+      <div
+        className="modal fade"
+        id="exampleModal"
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-dialog-centered mt-4" style={{ background: "rgba(0, 0, 0, 0.7)" }}>
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                Complete your Signup
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <div class="container-fluid">
+                <div class="row justify-content-center">
+                  <div class="col-md-6 ml-auto"><Image width="300px"
+                    height="300px"
+                    src='/assets/images/LoginPage.png'
+                    alt="login-page" /></div>
                 </div>
               </div>
+            </div>
 
-              <div className="modal-footer">
-                <Styles.ghostButton style={{
-                  background:"#111b2b",
-                  color: "white", display: "inline", paddingRight: "1.3rem", paddingLeft: "1.3rem", paddingTop: "10px",
-                  paddingBottom: "10px", fontSize: "17px", marginRight: "40px"
-                }}
-                  className="nav-link"
+            <div className="modal-footer">
+              <Styles.ghostButton style={{
+                background: "#111b2b",
+                color: "white", display: "inline", paddingRight: "1.3rem", paddingLeft: "1.3rem", paddingTop: "10px",
+                paddingBottom: "10px", fontSize: "17px", marginRight: "40px"
+              }}
+                className="nav-link"
                 onClick={() => location.push('/onboard/signup/company')}>Complete your Signup</Styles.ghostButton >
-              </div>
             </div>
           </div>
         </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
 }
+
+export default CompleteSignup;
