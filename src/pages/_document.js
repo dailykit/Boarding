40046ddb0cc02908323ada6c343c729db5a46c1f
rupalmalik3 <rupalmalik3@gmail.com>
2022-02-08 16:25:55 +0530

    feat: added facebook chat

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -19,6 +19,31 @@ class MyDocument extends Document {
           <script defer src="/analytics.js" />
           <script defer src="/facebook.js" />
           <script defer src="/googleTagManager.js" />
+          <div id="fb-root"></div>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.fbAsyncInit = function() {
+              FB.init({
+                xfbml            : true,
+                version          : 'v10.0'
+              });
+            };
+            (function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+            `,
+            }}
+          />
+
+          <div className="fb-customerchat"
+            attribution="page_inbox"
+            page_id="454878825266203">
+          </div>
         </body>
       </Html>
     );
